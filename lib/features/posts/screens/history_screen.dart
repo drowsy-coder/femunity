e8e0594fe3e3b4943ba68b22f988d0483af6c07b
Amritansh Sharma <amritansh.code@gmail.com>
2023-05-13 14:18:49 +0530

    Fixed history screen uid issue

--- lib/features/posts/screens/history_screen.dart
+++ lib/features/posts/screens/history_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HistoryScreen extends StatefulWidget {
   @override
@@ -8,6 +9,24 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
+  late String userId;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserInfo();
+  }
+
+  Future<void> getUserInfo() async {
+    // Get the current user id
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        userId = user.uid;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,91 +43,104 @@ class _HistoryScreenState extends State<HistoryScreen> {
             ? Colors.grey[900] // set color for dark mode
             : Color(0xFFAEC6CF),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
+      body: FutureBuilder<QuerySnapshot>(
+        future: FirebaseFirestore.instance
             .collection('period-tracker-data')
+            .where('userId', isEqualTo: userId)
             .orderBy('periodStartDate', descending: true)
-            .snapshots(),
+            .get(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
           if (!snapshot.hasData) {
             return Center(
               child: CircularProgressIndicator(),
             );
-          } else {
-            List<DocumentSnapshot> documents = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (BuildContext context, int index) {
-                DateTime periodStartDate =
-                    (documents[index].get('periodStartDate') as Timestamp)
-                        .toDate();
-                DateTime periodEndDate =
-                    (documents[index].get('periodEndDate') as Timestamp)
-                        .toDate();
-                String cycleLength = documents[index].get('cycleLength');
-                String periodLength = documents[index].get('periodLength');
-                bool periodOn = documents[index].get('periodOn');
-                DateTime nextPeriodDate =
-                    (documents[index].get('nextPeriodDate') as Timestamp)
-                        .toDate();
+          }
 
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 16.0),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    elevation: 8,
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.grey[900] // set color for dark mode
-                        : Color(0xFFAEC6CF),
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(16.0),
-                      onTap: () {
-                        // do something on tap
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${DateFormat.yMMMMd().format(periodStartDate)} - ${DateFormat.yMMMMd().format(periodEndDate)}',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Theme.of(context).brightness ==
-                                        Brightness.dark
-                                    ? Colors
-                                        .purpleAccent // set text color for dark mode
-                                    : Colors
-                                        .purpleAccent, // set text color for light mode
-                              ),
+          List<DocumentSnapshot> documents = snapshot.data!.docs;
+          if (documents.isEmpty) {
+            return Center(
+              child: Text('No data found'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (BuildContext context, int index) {
+              DateTime periodStartDate =
+                  (documents[index].get('periodStartDate') as Timestamp)
+                      .toDate();
+              DateTime periodEndDate =
+                  (documents[index].get('periodEndDate') as Timestamp)
+                      .toDate();
+              String cycleLength = documents[index].get('cycleLength');
+              String periodLength = documents[index].get('periodLength');
+              bool periodOn = documents[index].get('periodOn');
+              DateTime nextPeriodDate =
+                  (documents[index].get('nextPeriodDate') as Timestamp)
+                      .toDate();
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 8.0, horizontal: 16.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  elevation: 8,
+                  color: Theme.of(context).brightness == Brightness.dark
+                      ? Colors.grey[900] // set color for dark mode
+                      : Color(0xFFAEC6CF),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(16.0),
+                    onTap: () {
+                      // do something on tap
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${DateFormat.yMMMMd().format(periodStartDate)} - ${DateFormat.yMMMMd().format(periodEndDate)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors
+                                      .purpleAccent // set text color for dark mode
+                                  : Colors
+                                      .purpleAccent, // set text color for light mode
                             ),
-                            SizedBox(height: 8),
-                            Text(
-                              'Cycle length: $cycleLength days, Period length: $periodLength days',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Theme.of(context).brightness ==
-                                        Brightness.dark
-                                    ? Colors
-                                        .white // set text color for dark mode
-                                    : Colors
-                                        .black, // set text color for light mode
-                              ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Cycle length: $cycleLength days, Period length: $periodLength days',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors
+                                      .white // set text color for dark mode
+                                  : Colors
+                                      .black, // set text color for light mode
                             ),
-                            SizedBox(height: 8),
-                          ],
-                        ),
+                          ),
+                          SizedBox(height: 8),
+                        ],
                       ),
                     ),
                   ),
-                );
-              },
-            );
-          }
+                ),
+              );
+            },
+          );
         },
       ),
     );
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
   geolocator_windows
   permission_handler_windows
-  rive_common
   url_launcher_windows
 )
 
