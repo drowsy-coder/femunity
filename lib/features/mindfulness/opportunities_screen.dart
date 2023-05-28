90329049d205d5d4fdb26cdb3d73c98101608402
Amritansh Sharma <amritansh.code@gmail.com>
2023-05-28 15:35:01 +0530

    Fixed bug with opportunities_screen

--- lib/features/mindfulness/opportunities_screen.dart
+++ lib/features/mindfulness/opportunities_screen.dart
@@ -25,7 +25,7 @@ class _OpportunitiesScreenState extends State<OpportunitiesScreen> {
           'Students enrolled in an undergraduate or postgraduate degree will participate in unique educational experiences in areas ranging from core technical skills to holistic professional development.',
       'url': 'https://www.deshawindia.com/desis-ascend-educare/about.pdf',
       'image': 'https://avatars.githubusercontent.com/u/2298205?s=200&v=4',
-      'field': 'Business',
+      'field': 'Buisness',
     },
     {
       'title': 'Adobe Women in Tech',
@@ -80,106 +80,100 @@ class _OpportunitiesScreenState extends State<OpportunitiesScreen> {
                   autoPlayAnimationDuration: Duration(milliseconds: 800),
                   autoPlayCurve: Curves.fastOutSlowIn,
                 ),
-                items: scholarships.map((scholarship) {
-                  if (scholarship['field'] == selectedField) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                          margin: EdgeInsets.symmetric(horizontal: 8.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.shade200,
-                                blurRadius: 4.0,
-                                offset: Offset(0, 2),
+                items: filteredOpportunities.map((scholarship) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.shade200,
+                              blurRadius: 4.0,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(16.0),
+                              child: Image.network(
+                                scholarship['image'] ?? '',
+                                height: 200.0,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
                               ),
-                            ],
-                          ),
-                          child: Stack(
-                            children: [
-                              ClipRRect(
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(16.0),
-                                child: Image.network(
-                                  scholarship['image'] ?? '',
-                                  height: 200.0,
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
-                                ),
+                                color: Colors.black.withOpacity(0.1),
                               ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(16.0),
-                                  color: Colors.black.withOpacity(0.1),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: SingleChildScrollView(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        scholarship['title']?.toUpperCase() ??
-                                            '',
-                                        style: TextStyle(
-                                          fontSize: 24.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black,
-                                          fontFamily: 'Montserrat',
-                                        ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      scholarship['title']?.toUpperCase() ?? '',
+                                      style: TextStyle(
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black,
+                                        fontFamily: 'Montserrat',
                                       ),
-                                      SizedBox(height: 8.0),
-                                      Text(
-                                        scholarship['description'] ?? '',
-                                        style: TextStyle(
-                                          fontSize: 16.0,
-                                          color: Colors.black,
-                                          fontFamily: 'Montserrat',
-                                          fontWeight: FontWeight.bold,
-                                        ),
+                                    ),
+                                    SizedBox(height: 8.0),
+                                    Text(
+                                      scholarship['description'] ?? '',
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.black,
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.bold,
                                       ),
-                                    ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 16.0,
+                              right: 16.0,
+                              child: ElevatedButton(
+                                onPressed: () =>
+                                    launch(scholarship['url'] ?? ''),
+                                child: Text(
+                                  'Apply Now',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat',
+                                  ),
+                                ),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Theme.of(context).accentColor,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 24.0,
+                                    vertical: 12.0,
                                   ),
                                 ),
                               ),
-                              Positioned(
-                                bottom: 16.0,
-                                right: 16.0,
-                                child: ElevatedButton(
-                                  onPressed: () =>
-                                      launch(scholarship['url'] ?? ''),
-                                  child: Text(
-                                    'Apply Now',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Montserrat',
-                                    ),
-                                  ),
-                                  style: ElevatedButton.styleFrom(
-                                    primary: Theme.of(context).accentColor,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20.0),
-                                    ),
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 24.0,
-                                      vertical: 12.0,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  } else {
-                    return Container();
-                  }
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
                 }).toList(),
               ),
             ),
@@ -255,24 +249,21 @@ class _OpportunitiesScreenState extends State<OpportunitiesScreen> {
                     company: 'Facebook',
                     position: 'Developer Program Manager',
                     url: 'https://www.facebook.com/careers/jobs/',
-                    logo:
-                        'https://1000logos.net/wp-content/uploads/2021/10/logo-Meta.png',
+                    logo: 'assets/images/logo-Meta.png',
                   ),
                 if (selectedField == 'Business')
                   OpportunityTile(
                     company: 'Amazon',
                     position: 'Software Development Engineer',
                     url: 'https://www.amazon.jobs/en/',
-                    logo:
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2500px-Amazon_icon.svg.png',
+                    logo: 'assets/images/Amazon_icon.svg.png',
                   ),
                 if (selectedField == 'Law')
                   OpportunityTile(
                     company: 'Google',
                     position: 'Software Engineer',
                     url: 'https://www.google.com/about/careers/',
-                    logo:
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png',
+                    logo: 'assets/images/Google_ G _Logo.svg.webp',
                   ),
               ],
             ),
@@ -308,11 +299,7 @@ class OpportunityTile extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(
-              logo,
-              width: 60,
-              height: 60,
-            ),
+            Image.asset(logo, height: 60, width: 60),
             SizedBox(width: 16),
             Expanded(
               child: Column(
